7b1ca81e4219cb3fbdce706d7144dbdf46633699
Nico Varaschin <nicovaras22@gmail.com>
2017-01-21 13:22:17 -0300

    Ajax and loader gif

--- firefox_addon/popup/subte.js
+++ firefox_addon/popup/subte.js
@@ -1,32 +1,22 @@
-var div = document.createElement('div');
-div.textContent = "Cargando...";
-document.body.appendChild(div);
 $.ajax({
     url: "http://www.metrovias.com.ar",
     type: 'GET',
     success: function(data){
-        $("div").empty();
         var data = $(data);
         var lines = ["A", "B", "C", "D", "E", "H"];
         for (var i = 0; i < lines.length; ++i) {
-            var div = document.createElement('div');
-            var img = document.createElement('img');
-            var sp1 = document.createElement('span');
-            var sp2 = document.createElement('span');
-            img.src = "../icons/linea_" + lines[i] + ".png";
-            sp1.appendChild(img);
-            text = data.find("span#status-line-" + lines[i]).text();
+            var text = data.find("span#status-line-" + lines[i]).text();
+            var selector = $("div#" + lines[i] + " > span.status");
             if (text == "Normal" || text.indexOf("habitual")>-1){
-                sp2.textContent = "✓";
+                selector.text("✓");
+                selector.addClass('ok');
             } else if (text.indexOf("emora")>-1 || text.indexOf("obras")>-1){
-                sp2.textContent = "⚠";
+                selector.text("⚠");
+                selector.addClass('warn');
             } else {
-                sp2.textContent = "❌";
+                selector.text("❌");
+                selector.addClass('bad');
             }
-
-            div.appendChild(sp1);
-            div.appendChild(sp2);
-            document.body.appendChild(div);
         }
     }
 });
